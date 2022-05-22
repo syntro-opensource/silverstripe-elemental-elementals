d3f04d640f6ff9f9dabe9146431757ee690deec4
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2022-05-23 01:52:50 +0200

    add: new models for elements

---
+++ src/Model/Button.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Syntro\SilverstripeElementalElementals\Model;
+
+use SilverStripe\ORM\DataObject;
+use gorriecoe\Link\Models\Link;
+use gorriecoe\LinkField\LinkField;
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\FieldList;
+use Syntro\SilverStripeElementalBaseitem\Model\BaseItem;
+use Syntro\SilverstripeElementalElementals\Traits\TranslatableConfigArrays;
+
+/**
+ * A displayable Button
+ *
+ * @author Matthias Leutenegger <hello@syntro.ch>
+ */
+class Button extends BaseItem
+{
+    use TranslatableConfigArrays;
+    /**
+     * Defines the database table name
+     *  @var string
+     */
+    private static $table_name = 'ElementalsButton';
+
+    /**
+     * @config
+     * @var array
+     */
+     private static $styles = [];
+
+
+     /**
+      * Database fields
+      * @var array
+      */
+     private static $db = [
+         'Style' => 'Varchar',
+     ];
+
+    /**
+     * Has_one relationship
+     * @var array
+     */
+    private static $has_one = [
+        'Section' => DataObject::class,
+        'Link' => Link::class
+    ];
+
+    /**
+     * Relationship version ownership
+     * @var array
+     */
+    private static $owns = [
+        'Link'
+    ];
+
+    /**
+     * Defines summary fields commonly used in table columns
+     * as a quick overview of the data for this dataobject
+     * @var array
+     */
+    private static $summary_fields = [
+        'Link.Title' => 'Title',
+        'Style' => 'Style',
+    ];
+
+    /**
+     * CMS Fields
+     * @return FieldList
+     */
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->removeByName([
+            'SectionID',
+            'LinkID',
+            'Style',
+            'Title',
+        ]);
+
+        $fields->addFieldsToTab(
+            'Root.Main',
+            [
+                $linkField = LinkField::create(
+                    'Link',
+                    _t(__CLASS__ . '.LINKTITLE', 'Link'),
+                    $this
+                ),
+                $styleField = DropdownField::create(
+                    'Style',
+                    _t(__CLASS__ . '.STYLETITLE', 'Style'),
+                    $this->getTranslatedConfigArray('styles')
+                )
+            ],
+        );
+
+        return $fields;
+    }
+
+    public function getTitle()
+    {
+        return $this->LinkID ? $this->Link->Title : _t(__CLASS__ . '.UNTITLEDBUTTON', 'Untitled Button');
+    }
+}
---
+++ templates/Syntro/SilverstripeElementalElementals/Element/Spacer.ss
@@ -0,0 +1,3 @@
+<div class="$Spacing">
+
+</div>
